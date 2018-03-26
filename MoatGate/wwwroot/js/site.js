8b330cde03282f372c48f598c0ea8c34eff323cd
Rafael Vasconcelos <rafaelvascc@gmail.com>
2018-03-26 23:30:02 +0300

    added confirm to delete actions on index pages

--- MoatGate/wwwroot/js/site.js
+++ MoatGate/wwwroot/js/site.js
@@ -1,1 +1,25 @@
-﻿// Write your Javascript code.
+﻿$(function () {
+    appyConfirmPopoverToDeleteButtons();
+});
+
+function appyConfirmPopoverToDeleteButtons() {
+    var options = {
+        placement: "bottom",
+        selector: ".deletePopOver",
+        title: "Confirm Delete?",
+        trigger: "click",
+        html: true,
+        content: function (event) {
+            var $content = $(this).parents(".btn-group:first").siblings(".confirmDeletePopover").find("> div.btn-group").clone();
+            $(this).off("shown.bs.popover").on("shown.bs.popover", function (event) {
+                var $that = $(this);
+                $content.find(".btnCancelDelete").on("click", function () {
+                    $that.popover("hide");
+                });
+            });
+            return $content;
+        }
+    }
+
+    $("body").popover(options);
+}
