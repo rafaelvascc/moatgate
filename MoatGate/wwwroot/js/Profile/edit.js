d057825cc75897cf64516fb82a08fc488b129f85
Rafael Vasconcelos <rafaelvascc@gmail.com>
2019-06-09 22:43:30 -0300

    small fixes

--- MoatGate/wwwroot/js/Profile/edit.js
+++ MoatGate/wwwroot/js/Profile/edit.js
@@ -3,11 +3,8 @@
 
     $(".fa-info-circle[data-toggle='tooltip']").tooltip();
 
-    //https://harvesthq.github.io/chosen/
-    $(".applyChosen").chosen({ width: "100%" });
-
-    $(".applyChosen").on('change', function (evt, params) {
-        evt.currentTarget.value = params.selected;
+    $(".applySelectTwo").select2({
+        theme: "bootstrap"
     });
 
     $("#profile-img").off().on("click", function (e) {
